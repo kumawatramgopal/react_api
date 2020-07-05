068ddd6b74eca304d388f4b1cd9bffa4936a9bab
RamGopal <kumawatramgopal8401@gmail.com>
2020-07-05 11:28:55 +0530

    Updates

---
+++ precache-manifest.004215f909048cd926d959dc7a1bb2e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cccfe286827b8fb927d4dd5e9352d46",
+    "url": "/react_api/index.html"
+  },
+  {
+    "revision": "ab8b5e87e561505beb3c",
+    "url": "/react_api/static/css/main.6e4c8811.chunk.css"
+  },
+  {
+    "revision": "70cda94eb3362635337f",
+    "url": "/react_api/static/js/2.36f96040.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_api/static/js/2.36f96040.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab8b5e87e561505beb3c",
+    "url": "/react_api/static/js/main.4b0ee0b1.chunk.js"
+  },
+  {
+    "revision": "eb052397d6794090c583",
+    "url": "/react_api/static/js/runtime-main.c9458952.js"
+  }
+]);
